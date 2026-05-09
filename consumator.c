23cec23aa3111ace59f9e9da52fa54aea0eb218f
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2026-05-09 18:43:21 +0300

    Add files via upload

---
+++ consumator.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include"functii.h"
+
+int main()
+{
+	int id_mayproduce=msgget(MAY_PRODUCE, IPC_CREAT | 0666);
+        int id_mayconsume=msgget(MAY_CONSUME, IPC_CREAT | 0666);
+
+        if(id_mayproduce==-1 || id_mayconsume==-1)
+        {
+                fprintf(stderr,"EROARE LA DESCHIDEREA COZILOR DE MESAJE\n");
+                exit(1);
+        }
+	
+	struct mesaj msg;
+	msg.mtype=1;
+
+	while(1)
+	{
+		msgrcv(id_mayconsume,&msg,sizeof(msg.continut),msg.mtype,0);
+		printf("MESAJ PRIMIT: %s\n",msg.continut);
+		msgsnd(id_mayproduce,&msg,sizeof(msg.continut),0);
+	}
+
+	return 0;
+}
---
+++ functii.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+#define MAY_PRODUCE 1111
+#define MAY_CONSUME 2222
+
+#define CAPACITATE 5
+
+struct mesaj
+{
+	long mtype;
+	char continut[256];
+};
+
+
+
+#endif
